b21ce761ed445a51ceb93727f0f4dcf523cadcdf
Peter F Schulam <pschulam@gmail.com>
2013-02-13 23:10:39 -0500

    Backup push

--- technobabel/features/clusters.py
+++ technobabel/features/clusters.py
@@ -1,9 +1,5 @@
-clusDict = {}
-for line in open('joint_c100.txt','r'):
-    word, cluster = line.split()
-    clusDict[word] = cluster
+clusDict = dict(l.strip().split() for l in open('joint_c100.txt'))
     
 def clus_list(post):
-        
     for word in post['tokenized_text']:
         yield clusDict[word], 1
